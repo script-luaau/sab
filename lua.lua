35ef53e83f177217a7de0fb6cb95ee18dd6801c0
sabs-script <scriptdark37@gmail.com>
2025-12-31 03:05:22 +0800

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/admscript-lua/sab/refs/heads/main/luaaStil"))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/fSN6NtsnjbuSGv5m/raw", true))()
